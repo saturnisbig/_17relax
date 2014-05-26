873460c2cfc9a5e341b86fdadc2dd3fb28aad01c
Teddy <i.kenting@gmail.com>
2014-05-26 21:16:39 +0800

    suport admin app, and add some function

--- relax/admin.py
+++ relax/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from relax.models import News, Tag
 
 # Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tag', 'abstract', 'update_time', 'comment_num',
+                    'view_num')
+    search_fields = ('title', 'abstract', 'content')
+    list_filter = ('update_time', 'tag')
+    date_hierarchy = 'update_time'
+    ordering = ('-update_time',)
+    fields = ('title', 'tag', 'abstract', 'content', 'update_time',
+              'comment_num', 'view_num')
+    # django ticket 342, but also can't edit at when Add item.
+    #readonly_fields = ('tag', 'update_time', 'comment_num')
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['tag', 'update_time', 'comment_num']
+        else:
+            return []
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tagid', 'come_from')
+    ordering = ('-ctime',)
+
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Tag, TagAdmin)
--- relax/models.py
+++ relax/models.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=25, unique=True)
-    tagid = models.IntegerField(blank=True, default=0)
-    come_from = models.CharField(max_length=25, blank=True)
+    name = models.CharField("标签名", max_length=25, unique=True)
+    tagid = models.IntegerField("标签ID", blank=True, default=0)
+    come_from = models.CharField("来自哪", max_length=25, blank=True)
     ctime = models.DateTimeField(auto_now_add=True)
     #times update daily or weekly
 
@@ -13,17 +16,18 @@ class Tag(models.Model):
 
 
 class News(models.Model):
-    docid = models.CharField(max_length=30, unique=True)
-    title = models.CharField(max_length=100)
-    big_pic = models.URLField(blank=True)
-    list_pic = models.URLField(blank=True)
-    abstract = models.CharField(max_length=200, blank=True)
-    update_time = models.DateTimeField()
-    content = models.TextField()
-    comment_num = models.IntegerField(default=0)
+    docid = models.CharField("新闻ID", max_length=30, unique=True)
+    title = models.CharField("新闻标题", max_length=100)
+    big_pic = models.URLField("封面图片", blank=True)
+    list_pic = models.URLField("列表显示图片", blank=True)
+    abstract = models.CharField("新闻简介", max_length=200, blank=True)
+    update_time = models.DateTimeField("新闻更新时间", blank=True, null=True)
+    content = models.TextField("新闻内容")
+    comment_num = models.IntegerField("新闻评论数", default=0)
     ctime = models.DateTimeField(auto_now_add=True)
-    tag = models.ForeignKey(Tag)
-    view_num = models.IntegerField(default=1)
+    # foreign key must specify 'verbose_name' explicitly.
+    tag = models.ForeignKey(Tag, verbose_name="所属标签")
+    view_num = models.IntegerField("新闻查看数", default=1)
 
     def __unicode__(self):
         return self.title
